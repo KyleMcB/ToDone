286992f6256ed70083971c62e782ac827bc5fbf1
Kyle Mcburnett <kylemcburnett@xingpeds.com>
2021-10-13 21:18:31 -0700

    redraw screen when re-opening

--- app/src/main/java/com/xingpeds/todone/MainActivity.kt
+++ app/src/main/java/com/xingpeds/todone/MainActivity.kt
@@ -43,6 +43,10 @@ class MainActivity : ComponentActivity() {
                 }
         }
 
+    override fun onResume() {
+        super.onResume()
+        model.reComposeList()
+    }
     @ExperimentalMaterialApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
